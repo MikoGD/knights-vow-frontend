0aec8085f3062a857a37890b8d8c496ee8d2cdaa
Mikael Escolin <mikaelescolin@gmail.com>
2025-01-19 16:41:09 +0000

    Update app to use vue-router

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <script lang="ts" setup>
 import { onBeforeMount, ref } from 'vue';
-import PageLoader from '@components/loader/PageLoader.vue';
-import Authentication from './pages/authentication/sign-up/Authentication.vue';
 import { useEvents } from '@composables/useEvents';
 import { useAuthenticationStore } from './stores/authentication.store';
+import { RouterView, useRouter } from 'vue-router';
 
 const isAuthChecked = ref<boolean>(false);
 const store = useAuthenticationStore();
 const { subscribe } = useEvents();
+const router = useRouter();
 
 onBeforeMount(async () => {
   subscribe<{ userID: number }>('user-login', (data) => {
@@ -15,45 +15,37 @@ onBeforeMount(async () => {
       throw new Error('No user ID provided in event "user-login"');
     }
 
-    store.isAuthenticated = true;
     store.userID = data.userID;
     isAuthChecked.value = true;
+    router.push('/');
   });
-
-  await store.getAuthenicationStatus();
-  isAuthChecked.value = true;
 });
 </script>
 
 <template>
-  <!-- <nav>
-    <h1>Knights Vow</h1>
-  </nav> -->
   <main>
-    <Authentication v-if="isAuthChecked && !store.isAuthenticated" />
-    <h1 v-if="store.isAuthenticated">Logged in</h1>
+    <RouterView />
   </main>
-  <PageLoader :show="!isAuthChecked" />
 </template>
 
 <style lang="scss" scoped>
 @use '@styles/utils';
 @use '@styles/variables/colors';
 
-nav {
-  padding: 0.75rem 0.75rem;
-  border-bottom: 0.075rem solid #ccc;
+// nav {
+//   padding: 0.75rem 0.75rem;
+//   border-bottom: 0.075rem solid #ccc;
 
-  h1 {
-    font-weight: 400;
-    font-size: 1.5rem;
-  }
-}
+//   h1 {
+//     font-weight: 400;
+//     font-size: 1.5rem;
+//   }
+// }
 
 main {
   @include utils.center;
 
   height: 100vh;
-  padding: 2.5rem 2rem 0 2rem;
+  padding: 0 2rem;
 }
 </style>
---
+++ src/pages/Home.vue
@@ -0,0 +1,6 @@
+<script lang="ts" setup></script>
+<template>
+  <section class="home">
+    <h1>Logged in</h1>
+  </section>
+</template>
