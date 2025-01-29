2f0ae034849006a7e58be4d1d89c628e83aeab4b
Mikael Escolin <mikaelescolin@gmail.com>
2025-01-29 22:05:41 +0000

    Add SearchInput.vue

---
+++ src/components/SearchInput.vue
@@ -0,0 +1,52 @@
+<script lang="ts" setup>
+export interface FieldClasses {
+  field?: string;
+}
+
+const props = defineProps<{
+  id: string;
+  class?: string;
+  placeholder?: string;
+  disabled?: boolean;
+}>();
+const model = defineModel<string>({ required: true });
+const emits = defineEmits<{
+  focus: [FocusEvent];
+  blur: [FocusEvent];
+}>();
+</script>
+<template>
+  <input
+    :id="props.id"
+    v-model="model"
+    class="field__input"
+    type="text"
+    :class="props.class"
+    :placeholder="props.placeholder"
+    :disabled="props.disabled"
+    @blur="emits('blur', $event)"
+    @focus="emits('focus', $event)"
+  />
+</template>
+<style lang="scss" scoped>
+@use '@/styles/utils';
+@use '@/styles/variables/colors';
+
+.field {
+  @include utils.column;
+
+  min-height: 8.25rem;
+  width: 100%;
+
+  &__input {
+    height: 3.5rem;
+    width: 100%;
+    padding-left: 0.8rem;
+    padding-right: 0.8rem;
+    border: none;
+    border: 1px solid colors.$border-color;
+    border-radius: 0.3125rem;
+    font-size: 1rem;
+  }
+}
+</style>
