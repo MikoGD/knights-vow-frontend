2d63e82413520857a56c9ca8b57ec61a3ed8e9f1
Mikael Escolin <mikaelescolin@gmail.com>
2025-02-21 22:52:14 +0000

    Add delete file confirmation modal

---
+++ src/pages/Home/DeleteFileModal.vue
@@ -0,0 +1,26 @@
+<script lang="ts" setup>
+import Modal from '@/components/Modal.vue';
+
+const props = defineProps<{
+  show: boolean;
+  isLoading: boolean;
+}>();
+
+const emits = defineEmits<{
+  deleteFile: [];
+  close: [];
+}>();
+</script>
+<template>
+  <Modal
+    confirm
+    center-modal-content
+    header="Delete file"
+    :show="props.show"
+    :is-confirm-loading="props.isLoading"
+    @confirm="emits('deleteFile')"
+    @close="emits('close')"
+  >
+    <p>Are you sure you want to delete this file?</p>
+  </Modal>
+</template>
